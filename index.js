0e8648f1d5fb5aa9cfe9e78118e2708f00687700
Yunkyun Han <kyunbaragi@daum.net>
2017-08-03 13:52:16 +0900

    Server Test

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'kyun27801!',
+	database : 'piece_collector'
+});
+conn.connect(function(err) {
+    if (err) {
+        console.error('mysql connection error');
+        console.error(err);
+        throw err;
+    }
+});
+
 var app = express();
 
-app.get('/places', function(req, res) {
-	console.log("Requested");
-	res.send("HAHA");
+app.get('/places', function (req, res) {
+	console.log("Requested!");
+	var sql = 'SELECT * FROM places';
+	conn.query(sql, function (error, results, fields) {
+		if (error) {
+			console.log(error);
+			res.status(500).send('Internal Server Error');
+		} else {
+			res.json(results);		
+		}
+	}); 
 });
 
 app.listen(80, function(){
