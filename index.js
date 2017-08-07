4e0bb27877fbd68e4590a127dafab6bc96c014c6
YunKyun Han <kyunbaragi@daum.net>
2017-08-07 06:24:21 +0000

    Make upload image router by using multer module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 var mysql = require('mysql');
 var conn = mysql.createConnection({
 	host     : 'localhost',
@@ -17,17 +18,12 @@ conn.connect(function(err) {
 });
 
 var app = express();
-var jsonParser = bodyParser.json();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static('users'));
 
+var jsonParser = bodyParser.json();
 app.post('/users/new', jsonParser, function (req, res) {
 	console.log("/users/new requested");
-	var userID = req.body.id;
-	var userEmail = req.body.email;
-	console.log(userID + " / " + userEmail);
-});
-
-app.post('/users', jsonParser, function (req, res) {
-	console.log("/users requested");
 	var userID = req.body.id;
 	var userEmail = req.body.email;
 	var sql = "INSERT INTO users (_id, email, created) VALUES ('" + userID + "', '" + userEmail + "', NOW())";
@@ -39,10 +35,45 @@ app.post('/users', jsonParser, function (req, res) {
 			var dir = './users/' + userID;
 			if(!fs.existsSync(dir)) {
 				fs.mkdirSync(dir)
-			}	
+			}
 		}
-	});
+	})
 });
+
+
+var _storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'users/' + req.query.user_id + '/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
+})
+
+var upload = multer({ storage: _storage });
+app.post('/upload', upload.single('image'), function (req, res) {
+	console.log("/upload requested");
+	console.log('Uploaded:' + req.file.originalname);
+	
+	var userID = req.query.user_id;
+	var placeID = req.query.place_id;
+	var imageFile = req.file.originalname;
+	var sql = "INSERT INTO records (user_id, place_id, image_path, memo, created) VALUES ('"
+			+ userID + "', '"
+			+ placeID + "', '"
+			+ "http://128.199.209.152/images/" + userID + "/"  + imageFile + "/" + "', "
+			+ "NULL, "
+			+ "NOW())";
+	conn.query(sql, function (error, results, fields) {
+		if (error) {
+			console.log(error);
+			res.status(500).send('Internal Server Error');
+		} else {
+			res.status(200).send('Success');
+		}
+	});		
+});
+
 
 app.get('/places', function (req, res) {
 	console.log("/places requested");
